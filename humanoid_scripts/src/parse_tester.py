dcb1a23918316299b94c69eb8b1306ad49d1e000
Ruchi Bhalani <77944836+r-bhalani@users.noreply.github.com>
2021-05-16 22:48:08 -0500

    Delete parse_tester.py

--- humanoid_scripts/src/parse_tester.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#NOTE: to make executable, put in terminal:
-# chmod +x joints_demo.py
-# test the parser :)
-from parser import Parser
-parser = Parser("testem.bt")
-print(len(parser.parseFile()))
